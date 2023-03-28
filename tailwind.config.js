e98496f74a41cfd06fd5e1d5b624e0854984e689
Amin-GG <greengodgitgithub@gmail.com>
2023-03-28 16:36:08 +0330

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: { min: "320px", max: "480px" },
+        tablet: { min: "481px", max: "768px" },
+        laptob: { min: "769px", max: "1024px" },
+      },
+    },
   },
   plugins: [],
 };
